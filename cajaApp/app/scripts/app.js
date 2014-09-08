dcbeeb38be16bd36573ec81d6ff43175fda88cde
carlos <carlosthe19916@gmail.com>
2014-09-08 17:04:35 -0500

    1

---
+++ cajaApp/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cajaAppApp
+ * @description
+ * # cajaAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cajaAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ cajaApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cajaAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cajaAppApp
+ */
+angular.module('cajaAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
