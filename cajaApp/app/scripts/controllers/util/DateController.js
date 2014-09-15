0cb02628d7f98f635bee4292c8dffdb2181e181f
carlos <carlosthe19916@gmail.com>
2014-09-15 11:42:49 -0500

    1

--- cajaApp/app/scripts/controllers/util/DateController.js
+++ cajaApp/app/scripts/controllers/util/DateController.js
@@ -1,3 +1,17 @@
-/**
- * Created by carlos on 13/09/2014.
- */
+define(['../module'], function (controllers) {
+    'use strict';
+    controllers.controller('DateController', function($scope){
+
+        $scope.dateOptions = {
+            formatYear: 'yyyy',
+            startingDay: 1
+        };
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.opened = true;
+        };
+
+    });
+});
