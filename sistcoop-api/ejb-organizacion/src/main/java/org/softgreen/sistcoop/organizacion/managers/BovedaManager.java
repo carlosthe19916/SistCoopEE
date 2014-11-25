72a0c1e339b05afdfa8eb96fa8d69d2a4117295c
carlos <carlosthe19916@gmail.com>
2014-11-24 23:10:41 -0500

    3

--- sistcoop-api/ejb-organizacion/src/main/java/org/softgreen/sistcoop/organizacion/managers/BovedaManager.java
+++ sistcoop-api/ejb-organizacion/src/main/java/org/softgreen/sistcoop/organizacion/managers/BovedaManager.java
@@ -1,17 +1,11 @@
 package org.softgreen.sistcoop.organizacion.managers;
 
-import java.util.List;
-
+import javax.ejb.EJBException;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 
-import org.softgreen.sistcoop.organizacion.client.models.AgenciaModel;
 import org.softgreen.sistcoop.organizacion.client.models.BovedaModel;
-import org.softgreen.sistcoop.organizacion.client.models.CajaModel;
-import org.softgreen.sistcoop.organizacion.client.models.SucursalModel;
-import org.softgreen.sistcoop.organizacion.client.models.TrabajadorModel;
-import org.softgreen.sistcoop.organizacion.ejb.models.jpa.entities.BovedaCajaEntity;
 
 @Stateless
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
@@ -19,14 +13,15 @@ public class BovedaManager {
 
 	public boolean desactivarBoveda(BovedaModel model) {
 		if (model.isAbierto())
-			return false;
+			throw new EJBException("Boveda abierta, no se puede desactivar");
 
 		model.setEstado(false);
 		model.setEstadoMovimiento(false);
 		model.commit();
 
 		// verificar saldo de boveda este en 0
-
+		model.getBovedaCaja(cajaModel);
+		
 		// verificar que todas las cajas asociadas a la boveda esten cerradas
 
 		// eliminar todas las relaciones bovedacaja
